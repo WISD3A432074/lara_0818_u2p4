1472306d5168133570e5561c6726917cb85b15a6
LeoHuang <leohuang1996@gmail.com>
2017-11-28 18:21:08 +0800

    建立路由及PostsController及設定Controller回傳對應的view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,5 +6,29 @@
 
  class PostsController extends Controller
  {
-     //
+       /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index()
+        {
+            //
+            return view('index');
+        }
+        public function show()
+        {
+            //
+            return view('post');
+        }
+        public function about()
+        {
+            //
+            return view('about');
+        }
+        public function contact()
+        {
+            //
+            return view('contact');
+        }
  }
